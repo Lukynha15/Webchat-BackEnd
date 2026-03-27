15cedd5fb305c58a8c8d6ce5cd06eaa2bf99538f
Lukynha15 <lukynha48@gmail.com>
2026-03-26 21:30:07 -0300

    gateway ready

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,13 +1,30 @@
+import { OnModuleInit } from '@nestjs/common';
 import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @WebSocketGateway()
-export class MyGateway {
+export class MyGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log('a user connected', socket.id);
+    });
+  }
+
   @SubscribeMessage('newMessage')
   onNewMessage(@MessageBody() body: any) {
     console.log(body);
+
+    this.server.emit('onMessage', {
+      msg: 'New message',
+      content: body,
+    });
   }
 }
